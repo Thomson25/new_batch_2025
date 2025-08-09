91f644426fe903a182e62266b901fd3b19ec37ca
Thomson <114558216+Thomson25@users.noreply.github.com>
2025-08-09 19:21:33 +0530

    Update example.py

--- example.py
+++ example.py
@@ -1,1 +1,6 @@
-print('hello world')
+name=str(input('enter the name: '))
+print('to find factorial')        
+number=int(input('enter the number: '))
+factorial=number(number-1)
+print('the output is ',factorial)
+
